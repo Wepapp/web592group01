ed099665582dc6ec5af50f9522c003b512b8313a
Wepapp <ploys_4@hotmail.com>
2017-04-27 15:47:52 +0700

    Update main.php

--- main.php
+++ main.php
@@ -269,9 +269,7 @@
 				<div id="nav-dots" class="nav-dots">
 					<span class="nav-dot-current"></span>
 					
-					<span></span>
-					<span></span>
-					<span></span>
+					
 					
 				</div>
 
